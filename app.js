b6d946b823f0b04cd2a969206dd6571b527cbc13
AtanasovAlexandr <mephistosaa@gmail.com>
2020-03-08 12:22:53 +0300

    app.js was updated, production ready

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const authRoutes = require('./routes/auth.routes');
 const linksRoutes = require('./routes/link.routes');
@@ -13,6 +14,13 @@ app.use(express.json({ extended: true }));
 app.use('/api/auth', authRoutes);
 app.use('/api/link', linksRoutes);
 app.use('/t', redirectRoutes);
+
+if ( process.env.NODE_ENV === 'production' ) {
+  app.use('/', express.static( path.join(__dirname, 'client', 'build') ));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve( __dirname, 'client', 'build', 'index.html' ))
+  })
+};
 
 async function start() {
     try {
